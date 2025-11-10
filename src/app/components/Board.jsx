d21bcb4c97ca75ffd7dc8ab5cc94954b44880039
Sofia Benavides <Sbenavidesarias@usf.edu>
2025-11-09 22:34:01 -0500

    add Board componen with a basic state

--- src/app/components/Board.jsx
+++ src/app/components/Board.jsx
@@ -1,9 +1,19 @@
 'use client';
 
+'use client';
+
+import { useState, useEffect } from 'react';
+
 export default function Board() {
+  const [tickets, setTickets] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  // fetch ticket later
   return (
-    <div className="text-white">
-      Board component loaded.
-    </div>
+    <section className="mt-4 space-y-4">
+      <h2 className="text-xl font-semibold mb-2">Tickets</h2>
+      <p>Total tickets: {tickets.lenght}</p>
+    </section>
   );
 }
